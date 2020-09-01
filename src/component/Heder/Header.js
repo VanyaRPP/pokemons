a26811bfddea8e4447cce2e2afed1a3fff35aeec
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2020-09-01 22:45:41 +0300

    0

---
+++ src/component/Heder/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import s from "./styleHeader.module.css"
+
+
+const Header = () => {
+    return (
+        <div>
+            <div className={s.HEADER}>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header;
