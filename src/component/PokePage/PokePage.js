a524ebaca5adbf4bf6c3e2a9319ddd457b91fcd4
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2020-09-13 16:59:43 +0300

    Rel 1.0.1

--- src/component/PokePage/PokePage.js
+++ src/component/PokePage/PokePage.js
@@ -99,8 +99,14 @@ const PokePade = observer((props)=>{
       <>
         <Search
           placeholder="Search Pokemon"
-          onSearch={
-          value => setSearch(value)}
+          onSearch={()=>(
+            value => setSearch(value),
+            Search!==''?
+            gotoAllPage()
+            :
+            setCurrentPageUrl(currentPageUrl)
+          )
+          }
           onChange={e =>(
             setSearch(e.target.value)
           )}
