0c74e44b22699d4eea713bc2f5dc9aa14f31a5f8
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2020-09-14 02:53:05 +0300

    Updates

---
+++ precache-manifest.2539dccf6c1ed3c78e18481ae36c4408.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b94f072467055d6b11aa9fea548036",
+    "url": "/pokemons/index.html"
+  },
+  {
+    "revision": "8242c7836fa7b8ba5a0b",
+    "url": "/pokemons/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "7bb7188c24a543257312",
+    "url": "/pokemons/static/css/main.d66742d7.chunk.css"
+  },
+  {
+    "revision": "8242c7836fa7b8ba5a0b",
+    "url": "/pokemons/static/js/2.b11de8dc.chunk.js"
+  },
+  {
+    "revision": "f0d0855484a7a377ebb232589c642562",
+    "url": "/pokemons/static/js/2.b11de8dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb7188c24a543257312",
+    "url": "/pokemons/static/js/main.4bb8d1d9.chunk.js"
+  },
+  {
+    "revision": "b2629a5c54af193e8c39",
+    "url": "/pokemons/static/js/runtime-main.22858f6c.js"
+  }
+]);
